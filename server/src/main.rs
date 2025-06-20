8f814c910d0e229bfcac0d8796f30c265162ddfc
Ian Parra <ianparra200@gmail.com>
2025-06-20 18:33:02 -0400

    fix: use default configuration if file reading fails

--- server/src/main.rs
+++ server/src/main.rs
@@ -19,11 +19,22 @@ struct Cli {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     pretty_env_logger::init_timed();
-    let cli = Cli::parse();
-    let content = fs::read_to_string(cli.config_path).inspect_err(|e| error!("Error loading config file: {e}")).unwrap_or_default();
-    let config = config::load(&content)
-        .inspect_err(|e| error!("Error loading config: {e}. Loading default config."))
-        .unwrap_or_default();
+    let file_content = {
+        // Drop cli before server launch
+        let cli = Cli::parse();
+        info!("Reading config file:{}", cli.config_path);
+        fs::read_to_string(cli.config_path)
+    };
+    let config = match &file_content{
+        Ok(data) => {
+            config::load(data).inspect_err(|e| error!("Error loading config data: {e}.\nLoading default config.")).unwrap_or_default()
+        },
+        Err(e) => {
+            error!("Error loading config file: {e}.\nLoading default config.");
+            config::Config::default()
+        }
+    };
+  
     signaling::server::launch("0.0.0.0:4000", config, Box::new(move |token| {
         Box::pin(async move {
             use signaling::server::{UserCapabilities, UserInformation};
