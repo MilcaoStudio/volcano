349c6010e7e8a91a37f7bbae706c8e477a9ec4bc
Ian Parra <ianparra200@gmail.com>
2025-07-19 01:00:27 -0400

    chore(doc): api documentation

--- volcano-sfu/src/rtc/peer/api.rs
+++ volcano-sfu/src/rtc/peer/api.rs
@@ -3,13 +3,13 @@ use std::sync::Arc;
 use webrtc::{
     api::{
         interceptor_registry::register_default_interceptors,
-        media_engine::{MediaEngine, MIME_TYPE_OPUS, MIME_TYPE_VP8},
+        media_engine::MediaEngine,
         APIBuilder,
     },
     error::Result,
     interceptor::registry::Registry,
     peer_connection::{configuration::RTCConfiguration, RTCPeerConnection},
-    rtp_transceiver::rtp_codec::{RTCRtpCodecCapability, RTCRtpCodecParameters, RTCRtpHeaderExtensionCapability, RTPCodecType},
+    rtp_transceiver::rtp_codec::{RTCRtpHeaderExtensionCapability, RTPCodecType},
     sdp::extmap
 };
 
@@ -17,40 +17,14 @@ use crate::rtc::config::WebRTCTransportConfig;
 
 const FRAME_MARKING: &str = "urn:ietf:params:rtp-hdrext:framemarking";
 
-/// Initialise a new RTCPeerConnection
+/// Creates a new basic [RTCPeerConnection] using default codecs and interceptors.
+/// # Configuration
+/// Subscriber connection is configured with:
+/// - ICE Servers from [WebRTCTransportConfig::configuration]
 pub async fn create_subscriber_connection(cfg: &Arc<WebRTCTransportConfig>) -> Result<Arc<RTCPeerConnection>> {
     // Create a MediaEngine object to configure the supported codec
     let mut m = MediaEngine::default();
-    //m.register_default_codecs()?;
-    m.register_codec(
-        RTCRtpCodecParameters {
-            capability: RTCRtpCodecCapability {
-                mime_type: MIME_TYPE_VP8.to_owned(),
-                clock_rate: 90000,
-                channels: 0,
-                sdp_fmtp_line: "".to_owned(),
-                rtcp_feedback: vec![],
-            },
-            payload_type: 96,
-            ..Default::default()
-        },
-        RTPCodecType::Video,
-    )?;
-
-    m.register_codec(
-        RTCRtpCodecParameters {
-            capability: RTCRtpCodecCapability {
-                mime_type: MIME_TYPE_OPUS.to_owned(),
-                clock_rate: 48000,
-                channels: 2,
-                sdp_fmtp_line: "".to_owned(),
-                rtcp_feedback: vec![],
-            },
-            payload_type: 111,
-            ..Default::default()
-        },
-        RTPCodecType::Audio,
-    )?;
+    m.register_default_codecs()?;
    
     // Create a InterceptorRegistry. This is the user configurable RTP/RTCP Pipeline.
     // This provides NACKs, RTCP Reports and other features. If you use `webrtc.NewPeerConnection`
@@ -74,11 +48,16 @@ pub async fn create_subscriber_connection(cfg: &Arc<WebRTCTransportConfig>) -> R
         .map(Arc::new)
 }
 
+/// Creates a new [RTCPeerConnection] using the default codecs, default interceptos and the provided [WebRTCTransportConfig].
+/// # Configuration
+/// Publisher connection is configured with:
+/// - [WebRTCTransportConfig::setting] for the media engine.
+/// - [WebRTCTransportConfig::configuration] for the peer connection.
 pub async fn create_publisher_connection(cfg: WebRTCTransportConfig) -> Result<Arc<RTCPeerConnection>> {
     let mut m = MediaEngine::default();
     m.register_default_codecs()?;
    
-    set_header_extensions(&mut m)?;
+    set_header_extensions(&mut m);
 
     let setting_engine = cfg.setting.clone();
     let api = APIBuilder::new()
@@ -91,10 +70,14 @@ pub async fn create_publisher_connection(cfg: WebRTCTransportConfig) -> Result<A
 }
 
 /// Creates a new [RTCPeerConnection] using the default codecs, default interceptos and the provided [WebRTCTransportConfig].
+/// # Configuration
+/// Central connection is configured with:
+/// - [WebRTCTransportConfig::setting] for the media engine.
+/// - [WebRTCTransportConfig::configuration] for the peer connection.
 pub async fn create_central_connection(cfg: WebRTCTransportConfig) -> Result<Arc<RTCPeerConnection>> {
     let mut m = MediaEngine::default();
     m.register_default_codecs()?;
-    set_header_extensions(&mut m)?;
+    set_header_extensions(&mut m);
     let registry = register_default_interceptors(Registry::new(), &mut m)?;
     let setting_engine = cfg.setting.clone();
     let api = APIBuilder::new()
@@ -107,7 +90,7 @@ pub async fn create_central_connection(cfg: WebRTCTransportConfig) -> Result<Arc
         .map(Arc::new)
 }
 
-fn set_header_extensions(m: &mut MediaEngine) -> Result<()> {
+fn set_header_extensions(m: &mut MediaEngine) {
     let extensions_video = vec![
         extmap::SDES_MID_URI,
         extmap::SDES_RTP_STREAM_ID_URI,
@@ -116,14 +99,16 @@ fn set_header_extensions(m: &mut MediaEngine) -> Result<()> {
         FRAME_MARKING,
     ];
 
-    for extention in extensions_video {
-        m.register_header_extension(
+    for ext in extensions_video {
+        if let Err(err) = m.register_header_extension(
             RTCRtpHeaderExtensionCapability {
-                uri: String::from(extention),
+                uri: String::from(ext),
             },
             RTPCodecType::Video,
             None,
-        )?;
+        ) {
+            error!("Register ext {ext} failed: {err}");
+        };
     }
 
     let extensions_audio = vec![
@@ -132,15 +117,15 @@ fn set_header_extensions(m: &mut MediaEngine) -> Result<()> {
         extmap::AUDIO_LEVEL_URI,
     ];
 
-    for extention in extensions_audio {
-        m.register_header_extension(
+    for ext in extensions_audio {
+        if let Err(err) = m.register_header_extension(
             RTCRtpHeaderExtensionCapability {
-                uri: String::from(extention),
+                uri: String::from(ext),
             },
             RTPCodecType::Audio,
             None,
-        )?;
+        ) {
+            error!("Register ext {ext} failed: {err}");
+        };
     }
-
-    Ok(())
 }
